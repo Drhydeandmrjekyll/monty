5edf9ec89de17274e494e798e3b10855ea1f5689
thesimplezero <hi@thesimplezero.io>
2023-06-21 22:56:30 +0300

    monty fix

--- main.c
+++ main.c
@@ -1,74 +1,81 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 #include "monty.h"
 
-bus_t bus = {NULL, NULL, NULL, 0};
+#define MAX_LEN 1024
 
 /**
- * get_line - Reads a line from the given file
- * @line: A pointer to the buffer that will store the line
- * @file: The file from which to read the line
+ * is_number - Check if a string is a number
+ * @s: The string to check
  *
- * Return: The number of characters in the line, or -1 if the end of the file is reached
+ * Return: 1 if string is a number, 0 otherwise
  */
-ssize_t get_line(char **line, FILE *file)
+int is_number(char *s)
 {
-	int ch, size = 0, capacity = 1024;
-	char *buffer = malloc(capacity);
+    int i = 0;
 
-	if (!buffer)
-		return -1;
-
-	while ((ch = fgetc(file)) != '\n' && ch != EOF)
-	{
-		if (size + 1 >= capacity)
-		{
-			capacity *= 2;
-			buffer = realloc(buffer, capacity);
-			if (!buffer)
-				return -1;
-		}
-		buffer[size++] = ch;
-	}
-	buffer[size] = '\0';
-
-	*line = buffer;
-	return (ch == EOF && size == 0) ? -1 : size;
+    /* Handle negative numbers */
+    if (s[0] == '-')
+        i = 1;
+    for (; s[i]; i++)
+    {
+        if (!isdigit((unsigned char)s[i]))
+            return 0;
+    }
+    return 1;
 }
 
-int main(int argc, char **argv)
+/**
+ * main - Main entry point for the Monty interpreter.
+ * @argc: Number of arguments passed.
+ * @argv: Argument vector.
+ *
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure.
+ */
+int main(int argc, char *argv[])
 {
-	char *content = NULL;
-	FILE *file;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+    char line[MAX_LEN], *opcode = NULL, *arg = NULL;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    FILE *file;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    /* Check usage and open file */
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-	file = fopen(argv[1], "r");
-	bus.file = file;
+    /* Process each line in the file */
+    while (fgets(line, MAX_LEN, file))
+    {
+        line_number++;
+        opcode = strtok(line, " \t\n");
+        if (opcode == NULL || opcode[0] == '#')
+            continue;
+        if (strcmp(opcode, "push") == 0)
+        {
+            arg = strtok(NULL, " \t\n");
+            if (arg == NULL || is_number(arg) == 0)
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                free(stack);
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
+        }
+        /* TODO: Implement a function to find and execute the opcode */
+    }
 
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	while (read_line > 0)
-	{
-		read_line = get_line(&content, file);
-		bus.content = content;
-		counter++;
-		if (read_line >= 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-	return (0);
+    /* Cleanup */
+    free(stack);
+    fclose(file);
+    return EXIT_SUCCESS;
 }
